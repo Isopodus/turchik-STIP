f21e375aeeaa984d88910024dc111460c43f0122
Isopodus <isopodus@gmail.com>
2021-02-18 22:25:54 +0200

    Added lab1

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <title>Eugene Turchik STIP page</title>
-    </head>
-    <body>
-        <p>Initial commit</p>
-    </body>
-</html>
+<?php include_once("index.html"); ?>
